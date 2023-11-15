d8aa8ec005884a3dab8bd26b1a46d99cebd63aba
Alana Herbin-Debrincat <alana.hrb1@gmail.com>
2023-11-15 14:27:54 +0100

    ahah

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,13 @@
 /*   By: aherbin <aherbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 14:05:41 by aherbin           #+#    #+#             */
-/*   Updated: 2023/11/15 14:24:19 by aherbin          ###   ########.fr       */
+/*   Updated: 2023/11/15 14:27:42 by aherbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char	*tmp;
 

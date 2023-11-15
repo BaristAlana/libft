4dbdb807f5415f07d2fa9432f7b32d58760cad4d
Alana Herbin-Debrincat <alana.hrb1@gmail.com>
2023-11-15 14:25:07 +0100

    ahah

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: aherbin <aherbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 14:05:41 by aherbin           #+#    #+#             */
-/*   Updated: 2023/11/15 14:09:27 by aherbin          ###   ########.fr       */
+/*   Updated: 2023/11/15 14:24:19 by aherbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,13 @@
 
 void	ft_memmove(void *dest, const void *src, size_t n)
 {
-	char		*p;
-	const char	*q;
-	size_t		i;
+	char	*tmp;
 
-	q = (const char *)src + n - 1;
-	if (dest < src || dest >= src + n)
-	{
-		ft_memcpy(dest, src, n);
-	}
-	else
-	{
-		i = 0;
-		p = (char *)dest + n - 1;
-		while (i < n)
-		{
-			*p-- = *q--;
-			++i;
-		}
-	}
+	tmp = (char *)malloc(sizeof(char) * n);
+	if (tmp == NULL)
+		return (NULL);
+	ft_memcpy(tmp, src, n);
+	ft_memcpy(dest, tmp, n);
+	free(tmp);
+	return (dest);
 }

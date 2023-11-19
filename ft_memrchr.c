f377fbf82b8da72c32bd143e5f89e077fb2dbeef
Alana Herbin-Debrincat <alana.hrb1@gmail.com>
2023-11-19 23:06:21 +0100

    grrrr

--- ft_memrchr.c
+++ ft_memrchr.c
@@ -6,7 +6,7 @@
 /*   By: aherbin <aherbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 16:42:34 by aherbin           #+#    #+#             */
-/*   Updated: 2023/11/17 17:59:49 by aherbin          ###   ########.fr       */
+/*   Updated: 2023/11/19 23:05:02 by aherbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,40 +35,3 @@ void	*ft_memrchr(const void *s, int c, size_t n)
 		return (cmp);
 	return (NULL);
 }
-
-/*#include <stdio.h>
-#include <string.h>
-
-void	test_memrchr(const void *ptr, int value, size_t num) {
-	const unsigned char *result = ft_memrchr(ptr, value, num);
-
-	if (result != NULL) {
-		printf("Value '%c' found at position: %td\n", (char)value, result
-			- (const unsigned char *)ptr);
-	} else {
-		printf("Value '%c' not found.\n", (char)value);
-	}
-}
-
-int	main(void) {
-	char str[] = "Hello, world!";
-	int value_to_find = 'o';
-
-	printf("Testing 'memrchr' function:\n");
-
-	// Test cases
-	test_memrchr(str, value_to_find, strlen(str));
-		// Case 1: Value exists in the string
-	test_memrchr(str, 'x', strlen(str));
-		// Case 2: Value does not exist in the string
-	test_memrchr(str, '\0', strlen(str));
-		// Case 3: Null terminator character
-	test_memrchr(str, 'H', 1);
-		// Case 4: Search in the first character only
-	test_memrchr(str, '!', 1);
-		// Case 5: Search in the last character only
-	test_memrchr(str, '!', 0);                    // Case 6: Empty search space
-
-	return (0);
-}
-*/

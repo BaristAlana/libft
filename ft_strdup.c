5f1a455bab56bccb2ccf2bc1ac00363935f72945
Alana Herbin-Debrincat <alana.hrb1@gmail.com>
2023-11-19 22:36:43 +0100

    on y viens :3

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aherbin <aherbin@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: aherbin <aherbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 10:54:38 by aherbin           #+#    #+#             */
-/*   Updated: 2023/11/17 10:54:53 by aherbin          ###   ########.fr       */
+/*   Updated: 2023/11/19 22:36:22 by aherbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,25 @@
 char	*ft_strdup(const char *s)
 {
 	int		i;
-	int		n;
 	char	*str;
 
-	i = ft_strlen((char *)s) + 1;
-	n = 0;
-	if (!(malloc((sizeof str) * i + 1)))
-		return (0);
-	str = malloc((sizeof str) * i + 1);
-	while (s[n])
+	i = 0;
+	str = (char *)malloc((sizeof(char)) * ft_strlen(s) + 1);
+	if (!str)
+		return (NULL);
+	while (s[i])
 	{
-		str[n] = s[n];
-		++n;
+		str[i] = s[i];
+		++i;
 	}
-	str[n] = '\0';
+	str[i] = '\0';
 	return (str);
 }
-/*
-#include <stdio.h>
+
+/*#include <stdio.h>
 int main()
 {
   char *src = "ahahah";
   char *ptr = ft_strdup(src);
   printf("%s\n", ptr);
-}
-*/
+}*/
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aherbin <aherbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 18:07:04 by aherbin           #+#    #+#             */
-/*   Updated: 2023/11/18 18:49:32 by aherbin          ###   ########.fr       */
+/*   Updated: 2023/11/19 22:31:08 by aherbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*substr;
 
 	i = 0;
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
 	if (len > ft_strlen(s) - start)
 		len = ft_strlen(s) - start;
-	substr = (char *) malloc(len * sizeof(char));
+	substr = (char *) malloc(len * sizeof(char) + 1);
 	if (!substr)
 		return (NULL);
 	while (i < len)
@@ -28,6 +32,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		substr[i] = s[start + i];
 		++i;
 	}
+	substr[i] = 0;
 	return (substr);
 }
 

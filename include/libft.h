6da1135bc75c228481778ec9a3fad7ef7badd800
Alana Herbin-Debrincat <aherbin@student.42berlin.de>
2024-08-27 16:50:01 +0200

    Oh no

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: aherbin <aherbin@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 14:43:51 by aherbin           #+#    #+#             */
-/*   Updated: 2024/01/26 17:01:35 by aherbin          ###   ########.fr       */
+/*   Updated: 2024/08/27 16:49:45 by aherbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,8 @@ void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memrchr(const void *s, int c, size_t n);
 
 int		ft_atoi(char *str);
+
+long	ft_atol(char *str);
 
 char	*ft_strdup(const char *s);
 
